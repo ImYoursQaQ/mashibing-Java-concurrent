41dd5d2a8d54263b6917597905585eb2df8170aa
zhanghailiang <zhanghailiang@daddychoice.net>
2020-05-04 17:15:39 +0800

    countDownLatch的用法：实现每5个一批执行

---
+++ src/other/countDownLatch/LatchSample.java
@@ -0,0 +1,52 @@
+package other.countDownLatch;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 需要等待countDownLatch的计数为0，才能触发第二批事件的执行
+ */
+public class LatchSample {
+	public static void main(String[] args) throws InterruptedException {
+    	CountDownLatch latch = new CountDownLatch(6);
+           for (int i = 0; i < 5; i++) {
+                Thread t = new Thread(new FirstBatchWorker(latch));
+                t.start();
+    	}
+    	for (int i = 0; i < 5; i++) {
+        	    Thread t = new Thread(new SecondBatchWorker(latch));
+        	    t.start();
+    	}
+           // 注意这里也是演示目的的逻辑，并不是推荐的协调方式
+    	while ( latch.getCount() != 1 ){
+        	    Thread.sleep(100L);
+    	}
+    	System.out.println("Wait for first batch finish");
+    	latch.countDown();
+	}
+}
+class FirstBatchWorker implements Runnable {
+	private CountDownLatch latch;
+	public FirstBatchWorker(CountDownLatch latch) {
+    	this.latch = latch;
+	}
+	@Override
+	public void run() {
+        	System.out.println("First batch executed!");
+        	latch.countDown();
+	}
+}
+class SecondBatchWorker implements Runnable {
+	private CountDownLatch latch;
+	public SecondBatchWorker(CountDownLatch latch) {
+    	this.latch = latch;
+	}
+	@Override
+	public void run() {
+    	try {
+        	latch.await();
+        	System.out.println("Second batch executed!");
+    	} catch (InterruptedException e) {
+        	e.printStackTrace();
+    	}
+	}
+}
